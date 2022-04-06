13d18bd258272e136a3593928b8dcb85d22a55fd
AbnerJosuEstrada <josueestrada@twowheelstogo.com>
2022-04-06 11:19:23 -0600

    test new version react

--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,38 @@
-import NextApp from "next/app";
-import React from "react";
-import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
-//import { SnackbarProvider } from "notistack";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { ContextProviders } from "context/ContextProviders";
-import { ComponentsProvider } from "@reactioncommerce/components-context";
-import components from "custom/componentsContext";
-import theme from "custom/reactionTheme";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+/* import theme from '../src/theme'; */
+/* import createEmotionCache from '../src/createEmotionCache'; */
+/* import { SnackbarProvider } from "notistack"; */
 
-import { StripeWrapper } from "components/StripeCard";
-import 'rc-collapse/assets/index.css';
+// Client-side cache, shared for the whole session of the user in the browser.
+/* const clientSideEmotionCache = createEmotionCache();
+ */
+export default function MyApp(props) {
+  const { Component,/*  emotionCache = clientSideEmotionCache */ pageProps } = props;
 
-export default class App extends NextApp {
-  componentDidMount() {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles && jssStyles.parentNode) {
-      jssStyles.parentNode.removeChild(jssStyles);
-    }
-  }
-
-  render() {
-    const { Component, pageProps, ...rest } = this.props;
-
-    return (
-      <StripeWrapper>
-        <ContextProviders pageProps={pageProps}>
-          <ComponentsProvider value={components}>
-            <MuiThemeProvider theme={theme}>
-              <CssBaseline />
-              <Component {...rest} {...pageProps} />
-            </MuiThemeProvider>
-          </ComponentsProvider>
-        </ContextProviders>
-      </StripeWrapper>
-    );
-  }
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider /* theme={theme} */>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+  {/*       <SnackbarProvider maxSnack={3}> */}
+  <Component {...rest} {...pageProps} />
+         {/* <Component {...pageProps} /> */}
+  {/*       </SnackbarProvider> */}
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+/*   emotionCache: PropTypes.object, */
+  pageProps: PropTypes.object.isRequired,
+};
